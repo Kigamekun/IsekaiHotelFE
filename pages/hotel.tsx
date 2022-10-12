f653f3707a8f360328943b856c22bf90bac53a74
Kigamekun <reksa.prayoga1012@gmail.com>
2022-10-12 12:05:26 +0700

    feat(slicing)

--- pages/hotel.tsx
+++ pages/hotel.tsx
@@ -1,18 +1,84 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import { useRouter } from 'next/router'
 import Image from 'next/image'
 import styles from '../styles/hotel.module.css'
 import 'bootstrap/dist/css/bootstrap.css'
 import Navbar from './components/Navbar'
-import Footer  from './components/Footer'
+import Footer from './components/Footer'
+import React, { useEffect, useState } from "react"
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import axios from 'axios'
+import $ from 'jquery'
+
+import Parser from 'html-react-parser';
 
 const Hotel: NextPage = () => {
+
+  const router = useRouter()
+  const data = router.query;
+
+  const [datas, setData] = useState<any[]>([]);
+  const [modalDetail, setModalDetail] = useState<string>('Loading ...');
+
+  const getRoomData = async () => {
+    var res = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/room?id=${data.hotel}`)
+      .then(function (response) {
+        setData(response.data.data.data);
+      }).catch(function (error) {
+
+      })
+  }
+
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = (e: any) => {
+
+
+    var html = `
+    <div className="modal-content">
+      <div className="modal-body p-5">
+      <img class="w-100 mb-4" style="border-radius:10px;" src="${e.currentTarget.getAttribute('data-thumb')}"/>
+      <h3 ><b>${e.currentTarget.getAttribute('data-name')}</b></h3  >
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero tempora beatae officia quisquam ab impedit facilis explicabo dignissimos nostrum, blanditiis cum cumque nihil? Animi ut harum adipisci cupiditate aliquid non asperiores quasi dolor vitae maxime, molestias eum officiis minus maiores.  </p>
+        <br />
+        <br />
+        <br />
+        <div className="d-flex justify-content-end gap-4">
+          <button type="button" className="btn button-left" data-bs-dismiss="modal">Close</button>
+          <button type="button" className="btn button-right">Book !</button>
+        </div>
+
+        </div>
+
+    </div>
+  
+  `;
+    setModalDetail(html);
+    console.log(modalDetail);
+    setShow(true);
+
+  };
+
+
+  useEffect(() => {
+    getRoomData();
+  }, [data]);
+
+
+
+  const getDataModal = async (e: any) => {
+
+  }
+
   return (
     <>
       <div className={styles.hero}>
         <Navbar></Navbar>
         <div className={styles.heroContent}>
-          <div className={styles.heroSection}>  
+          <div className={styles.heroSection}>
           </div>
         </div>
       </div>
@@ -24,57 +90,31 @@ const Hotel: NextPage = () => {
       <br />
       <br />
       <h2 style={{ textAlign: 'center' }}><b>List Room</b></h2>
+      <br />
+      <br />
+      <div className="container" style={{ background: '#BDD5D7', display: 'flex', gap: '20px', justifyContent: 'center', flexWrap: 'wrap', padding: '50px', borderRadius: '20px' }}>
+        {datas &&
+          datas.map((data, key) => (
+            <div key={key} onClick={handleShow} className={styles.cards} data-name={data.name} data-thumb={data.thumb}>
+              <img src={data.thumb} className="card-img-top" style={{ height: "200px" }} />
+              <h5><b>{data.name}</b></h5>
+              <h6>${data.price}</h6>
+            </div>
 
-      <br />
-      <br />
-      <div className="container" style={{ background: '#BDD5D7',display:'flex',gap:'20px',justifyContent:'center',flexWrap:'wrap' ,padding:'50px',borderRadius:'20px'}}>
-        <div className={styles.cards}>
-          <img src="/img/room1.jpg" className="card-img-top" style={{ height: "200px" }} />
-          <h5><b>Kawakai Room</b></h5>
-          <h6>$36</h6>
-        </div>
-        <div className={styles.cards}>
-          <img src="/img/room1.jpg" className="card-img-top" style={{ height: "200px" }} />
-          <h5><b>Kawakai Room</b></h5>
-          <h6>$36</h6>
-        </div>
-        <div className={styles.cards}>
-          <img src="/img/room1.jpg" className="card-img-top" style={{ height: "200px" }} />
-          <h5><b>Kawakai Room</b></h5>
-          <h6>$36</h6>
-        </div>
-        <div className={styles.cards}>
-          <img src="/img/room1.jpg" className="card-img-top" style={{ height: "200px" }} />
-          <h5><b>Kawakai Room</b></h5>
-          <h6>$36</h6>
-        </div>
-        <div className={styles.cards}>
-          <img src="/img/room1.jpg" className="card-img-top" style={{ height: "200px" }} />
-          <h5><b>Kawakai Room</b></h5>
-          <h6>$36</h6>
-        </div>
-        <div className={styles.cards}>
-          <img src="/img/room1.jpg" className="card-img-top" style={{ height: "200px" }} />
-          <h5><b>Kawakai Room</b></h5>
-          <h6>$36</h6>
-        </div>
-        <div className={styles.cards}>
-          <img src="/img/room1.jpg" className="card-img-top" style={{ height: "200px" }} />
-          <h5><b>Kawakai Room</b></h5>
-          <h6>$36</h6>
-        </div>
-        <div className={styles.cards}>
-          <img src="/img/room1.jpg" className="card-img-top" style={{ height: "200px" }} />
-          <h5><b>Kawakai Room</b></h5>
-          <h6>$36</h6>
-        </div>
-        <div className={styles.cards}>
-          <img src="/img/room1.jpg" className="card-img-top" style={{ height: "200px" }} />
-          <h5><b>Kawakai Room</b></h5>
-          <h6>$36</h6>
-        </div>
+          ))}
       </div>
       <Footer></Footer>
+
+
+
+
+
+
+
+
+      <Modal size="lg" onEntered={getDataModal} show={show} onHide={handleClose}>
+        {Parser(modalDetail)}
+      </Modal>
     </>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,76 @@
 import type { NextPage } from 'next'
+import React, { useEffect, useState } from "react"
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/index.module.css'
 import 'bootstrap/dist/css/bootstrap.css'
-import Navbar  from './components/Navbar'
+import Navbar from './components/Navbar'
+import axios from 'axios'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as Yup from 'yup';
+
+type UserSubmitForm = {
+  start_from: Date;
+  end_at: Date;
+  hotel: number;
+  count: number;
+};
+
 
 const App: NextPage = () => {
+  const [datas, setData] = useState<any[]>([]);
+
+  const getHotelData = async () => {
+    var res = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/hotel`)
+      .then(function (response) {
+        setData(response.data.data.data);
+      }).catch(function (error) {
+
+      })
+  }
+
+  const validationSchema = Yup.object().shape({
+    start_from: Yup.string().required('Fullname is required'),
+    end_at: Yup.string().required('Username is required'),
+    hotel: Yup.number().required('Email is required'),
+    count: Yup.number().required('Email is required'),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors }
+  } = useForm<UserSubmitForm>({
+    resolver: yupResolver(validationSchema)
+  });
+
+  const onSubmit = (data: UserSubmitForm) => {
+    router.push({
+      pathname: '/hotel',
+      query: {
+        start_from: (document.getElementById('start_from') as HTMLInputElement).value,
+        end_at: (document.getElementById('end_at') as HTMLInputElement).value,
+        hotel: (document.getElementById('hotel') as HTMLInputElement).value,
+        count: (document.getElementById('count') as HTMLInputElement).value,
+      },
+    })
+  };
+
+  const router = useRouter()
+
+
+  useEffect(() => {
+    getHotelData();
+  }, []);
+
   return (
     <>
       <div className={styles.hero}>
-      <Navbar></Navbar>
+        <Navbar></Navbar>
         <div className={styles.heroContent}>
           <div>
             <h1><b>Find Your</b></h1>
@@ -37,21 +98,25 @@ const App: NextPage = () => {
       </div>
       <br />
       <div className={styles.containerBook}>
-        <div className={styles.inputBook}>
+        <form className={styles.inputBook} onSubmit={handleSubmit(onSubmit)}>
           <div className={styles.inputBookForm}>
-            <input type="date" className="form-control" style={{ width: '245px' ,display: "inline-block", padding: "15px 10px", lineHeight: "140%"}} />
-            <input type="date" className="form-control" style={{ width: '245px' ,display: "inline-block", padding: "15px 10px", lineHeight: "140%"}} />
-            <select name="" id="" className="form-select" style={{ width: '245px' ,display: "inline-block", padding: "15px 10px", lineHeight: "140%"}}>
+            <input type="date" id='start_from' {...register('start_from')} className={`form-control ${errors.start_from ? 'is-invalid' : ''}`} style={{ width: '245px', display: "inline-block", padding: "15px 10px", lineHeight: "140%" }} />
+            <input type="date" id='end_at' {...register('end_at')} className={`form-control ${errors.end_at ? 'is-invalid' : ''}`} style={{ width: '245px', display: "inline-block", padding: "15px 10px", lineHeight: "140%" }} />
+            <select id="hotel" {...register('hotel')} className={`form-control ${errors.hotel ? 'is-invalid' : ''}`} style={{ width: '245px', display: "inline-block", padding: "15px 10px", lineHeight: "140%" }}>
               <option value="">Select Hotel</option>
+              {datas &&
+                datas.map((dt, key) => (
+                  <option value={dt.id} key={key}>{dt.name}</option>
+                ))}
             </select>
-            <input type="number" className="form-control" value={0} style={{ width: '75px' ,display: "inline-block", padding: "15px 10px", lineHeight: "140%"}} />
+            <input type="number" id='count' {...register('count')} className={`form-control ${errors.count ? 'is-invalid' : ''}`} min="0" style={{ width: '75px', display: "inline-block", padding: "15px 10px", lineHeight: "140%" }} />
           </div>
           <div>
-            <button className="btn" style={{borderRadius:"20px",fontSize:"32px",color:'#53A1D0',height:'140px',width:'330px',background:'white'}}>
+            <button type="submit" className="btn" style={{ borderRadius: "20px", fontSize: "32px", color: '#53A1D0', height: '140px', width: '330px', background: 'white' }}>
               <b>Book Now !</b>
             </button>
           </div>
-        </div>
+        </form>
       </div>
     </>
   )
